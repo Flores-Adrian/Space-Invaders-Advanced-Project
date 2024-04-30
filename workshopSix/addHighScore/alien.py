309410ec4fa33d4d6206fae6f2e35d56edfcda91
Flores-Adrian <95241777+Flores-Adrian@users.noreply.github.com>
2024-04-30 00:12:40 -0700

    Create alien.py

---
+++ workshopSix/addHighScore/alien.py
@@ -0,0 +1,47 @@
+import pygame, random
+
+class Alien(pygame.sprite.Sprite):
+
+    def __init__(self, type, x,y):
+        super().__init__()
+
+        self.type = type
+
+        #path file
+        path = f"Images/alien_{type}.png"
+        self.image = pygame.image.load(path)
+        self.rect = self.image.get_rect(topleft = (x,y))
+
+    def update(self, direction):
+        self.rect.x += direction
+
+
+class MysteryShip(pygame.sprite.Sprite):
+
+    def __init__(self, screen_width, offset):
+        super().__init__()
+
+        self.offset = offset
+
+        self.screen_width = screen_width
+
+        self.image = pygame.image.load("Images/mystery.png")
+
+        x = random.choice([self.offset/2, screen_width + self.offset - self.image.get_width()])
+
+        if x == self.offset/2:
+            self.speed = 3
+        else:
+            self.speed = -3
+
+        self.rect = self.image.get_rect(topleft = (x, 40))
+
+    def update(self):
+        self.rect.x += self.speed
+
+        # delete spaceship if it is outside of the game screen
+
+        if self.rect.right > self.screen_width + self.offset/2:
+            self.kill()
+        elif self.rect.left < self.offset/2:
+            self.kill()
